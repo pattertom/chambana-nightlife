2185d810c9f9c0ad5b8af837fcde012526cc9d03
Tom Patterson <tom@businesslogic.com>
2010-03-30 10:35:33 -0500

    fixing map colors
    Worked on by: pattertom

--- system/application/views/Bar/Bar_single_view.php
+++ system/application/views/Bar/Bar_single_view.php
@@ -154,86 +154,88 @@ foreach($reviews->result() as $row)
 	<h1 class="underlined">Address</h2>
     <div class="paragraph"><?php echo $bar->address?></div>
     
-<!-- ++Begin Map Search Control Wizard Generated Code++ -->
-  <!--
-  // Created with a Google AJAX Search Wizard
-  // http://code.google.com/apis/ajaxsearch/wizards.html
-  -->
+    <div class="map">
+        <!-- ++Begin Map Search Control Wizard Generated Code++ -->
+        <!--
+        // Created with a Google AJAX Search Wizard
+        // http://code.google.com/apis/ajaxsearch/wizards.html
+        -->
 
-  <!--
-  // The Following div element will end up holding the map search control.
-  // You can place this anywhere on your page
-  -->
-  <div id="mapsearch">
-    <span style="color:#676767;font-size:11px;margin:10px;padding:4px;">Loading...</span>
-  </div>
+        <!--
+        // The Following div element will end up holding the map search control.
+        // You can place this anywhere on your page
+        -->
+        <div id="mapsearch">
+        <span style="color:#676767;font-size:11px;margin:10px;padding:4px;">Loading...</span>
+        </div>
 
-  <!-- Maps Api, Ajax Search Api and Stylesheet
-  // Note: If you are already using the Maps API then do not include it again
-  //       If you are already using the AJAX Search API, then do not include it
-  //       or its stylesheet again
-  //
-  // The Key Embedded in the following script tags is designed to work with
-  // the following site:
-  // http://localhost/
-  -->
-  <script src="http://maps.google.com/maps?file=api&v=2&key=ABQIAAAARWVeFhNhrPu-BpalRmnZ8xT2yXp_ZAY8_ufC3CFXhHIE1NvwkxSYPfXl4SnjiuOj6HbyX2_IsjgutA"
-    type="text/javascript"></script>
-  <script src="http://www.google.com/uds/api?file=uds.js&v=1.0&source=uds-msw&key=ABQIAAAARWVeFhNhrPu-BpalRmnZ8xT2yXp_ZAY8_ufC3CFXhHIE1NvwkxSYPfXl4SnjiuOj6HbyX2_IsjgutA"
-    type="text/javascript"></script>
-  <style type="text/css">
-    @import url("http://www.google.com/uds/css/gsearch.css");
-  </style>
+        <!-- Maps Api, Ajax Search Api and Stylesheet
+        // Note: If you are already using the Maps API then do not include it again
+        //       If you are already using the AJAX Search API, then do not include it
+        //       or its stylesheet again
+        //
+        // The Key Embedded in the following script tags is designed to work with
+        // the following site:
+        // http://localhost/
+        -->
+        <script src="http://maps.google.com/maps?file=api&v=2&key=ABQIAAAARWVeFhNhrPu-BpalRmnZ8xT2yXp_ZAY8_ufC3CFXhHIE1NvwkxSYPfXl4SnjiuOj6HbyX2_IsjgutA"
+        type="text/javascript"></script>
+        <script src="http://www.google.com/uds/api?file=uds.js&v=1.0&source=uds-msw&key=ABQIAAAARWVeFhNhrPu-BpalRmnZ8xT2yXp_ZAY8_ufC3CFXhHIE1NvwkxSYPfXl4SnjiuOj6HbyX2_IsjgutA"
+        type="text/javascript"></script>
+        <style type="text/css">
+        @import url("http://www.google.com/uds/css/gsearch.css");
+        </style>
 
-  <!-- Map Search Control and Stylesheet -->
-  <script type="text/javascript">
-    window._uds_msw_donotrepair = true;
-  </script>
-  <script src="http://www.google.com/uds/solutions/mapsearch/gsmapsearch.js?mode=new"
-    type="text/javascript"></script>
-  <style type="text/css">
-    @import url("http://www.google.com/uds/solutions/mapsearch/gsmapsearch.css");
-  </style>
+        <!-- Map Search Control and Stylesheet -->
+        <script type="text/javascript">
+        window._uds_msw_donotrepair = true;
+        </script>
+        <script src="http://www.google.com/uds/solutions/mapsearch/gsmapsearch.js?mode=new"
+        type="text/javascript"></script>
+        <style type="text/css">
+        @import url("http://www.google.com/uds/solutions/mapsearch/gsmapsearch.css");
+        </style>
 
-  <style type="text/css">
-    .gsmsc-mapDiv {
-      height : 300px;
-    }
+        <style type="text/css">
+        .gsmsc-mapDiv {
+          height : 300px;
+        }
 
-    .gsmsc-idleMapDiv {
-      height : 200px;
-    }
+        .gsmsc-idleMapDiv {
+          height : 200px;
+        }
 
-    #mapsearch {
-      width : 300px;
-      margin: 10px;
-      padding: 4px;
-    }
-  </style>
-  <script type="text/javascript">
-    function LoadMapSearchControl() {
+        #mapsearch {
+          width : 300px;
+          margin: 10px;
+          padding: 4px;
+        }
+        </style>
+        <script type="text/javascript">
+        function LoadMapSearchControl() {
 
-      var options = {
-            zoomControl : GSmapSearchControl.ZOOM_CONTROL_ENABLE_ALL,
-            title : "<?php echo $bar->name ?>",
-            <!-- Need to specify a url for the bar otherwise the title does not show up -->
-            url : "http://localhost/chambana-nightlife/index.php/bar/viewBar/<?php echo $bar->name ?>",
-            idleMapZoom : GSmapSearchControl.ACTIVE_MAP_ZOOM,
-            activeMapZoom : GSmapSearchControl.ACTIVE_MAP_ZOOM
-            }
+          var options = {
+                zoomControl : GSmapSearchControl.ZOOM_CONTROL_ENABLE_ALL,
+                title : "<?php echo $bar->name ?>",
+                <!-- Need to specify a url for the bar otherwise the title does not show up -->
+                url : "http://localhost/chambana-nightlife/index.php/bar/viewBar/<?php echo $bar->name ?>",
+                idleMapZoom : GSmapSearchControl.ACTIVE_MAP_ZOOM,
+                activeMapZoom : GSmapSearchControl.ACTIVE_MAP_ZOOM
+                }
 
-      new GSmapSearchControl(
-            document.getElementById("mapsearch"),
-            "<?php echo $bar->address ?>",
-            options
-            );
+          new GSmapSearchControl(
+                document.getElementById("mapsearch"),
+                "<?php echo $bar->address ?>",
+                options
+                );
 
-    }
-    // arrange for this function to be called during body.onload
-    // event processing
-    GSearch.setOnLoadCallback(LoadMapSearchControl);
-  </script>
-<!-- ++End Map Search Control Wizard Generated Code++ --> 
+        }
+        // arrange for this function to be called during body.onload
+        // event processing
+        GSearch.setOnLoadCallback(LoadMapSearchControl);
+        </script>
+        <!-- ++End Map Search Control Wizard Generated Code++ --> 
+    </div>
             
 	
 </div>
