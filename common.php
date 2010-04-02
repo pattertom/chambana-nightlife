63c02f29183bbbafad1e5c1942d610e1a2a98104
Tom Patterson <tom@businesslogic.com>
2010-04-01 19:50:16 -0500

    quickie
    Worked on by: pattertom

--- common.php
+++ common.php
@@ -1,9 +1,9 @@
 <?php
 function get_new_dimensions($height, $width, $pixel_height) {
-    // if ($height > $width)
+    if ($height > $width)
         $scale = $height > $pixel_height ? $pixel_height/$height : 1;
-    // else
-        // $scale = $width > $pixel_height ? $pixel_height/$width : 1;
+    else
+        $scale = $width > $pixel_height ? $pixel_height/$width : 1;
     return array($scale*$height, $scale*$width, $scale);
 }
 ?>
