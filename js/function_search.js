191604ec23c0b3328e2df76ebd60d7926a0b0971
unknown <Kartikeya Dubey@.(none)>
2010-04-25 15:10:02 -0600

    added some js files

---
+++ js/function_search.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    
+             new Ajax.Autocompleter("function_name", "autocomplete_choices", "http://localhost/chambana-nightlife/index.php/bar/ajaxsearch/", {});
+}
+
